e42a724a0497347a47d99e6359f013f672355889
amaelDago <amaeldago@gmail.com>
2021-09-02 14:25:15 +0200

    fix: Replace conf by request_elastic

--- pythonScripts/deduplicate/request_elastic.py
+++ pythonScripts/deduplicate/request_elastic.py
@@ -1,12 +1,15 @@
-#import requests
-from .utils import NoticeComparison
+
+import os
+from utils import NoticeComparison
 from elasticsearch import RequestsHttpConnection
 from elasticsearch import Elasticsearch
-from .params import titleStopwords
-from nltk import word_tokenize
+from params import titleStopwords
 from nltk.stem import SnowballStemmer
 from nltk.stem import PorterStemmer
 from langdetect import detect
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Instaciantye langdetctor
@@ -16,11 +19,12 @@ en = PorterStemmer()
 
 # Set connection config
 
-esUrl = "http://vp-conditor-es.intra.inist.fr:9200/"
-index = "records-202012"
-doc_type = "record"
-proxies = {'https': 'http://proxyout.inist.fr:8080/', "http" : 'http://proxyout.inist.fr:8080/'}
-size = 100
+esUrl = os.environ.get("ES_URL", "http://localhost:9200")
+index = os.environ.get("INDEX")
+doc_type = os.environ.get("RECORD")
+proxies = os.environ.get("PROXIES")
+size =os.environ["RESPONSE_SIZE"]
+
 
 
 class MyConnection(RequestsHttpConnection):
@@ -30,7 +34,7 @@ class MyConnection(RequestsHttpConnection):
         self.session.proxies = proxies
 
 
-class ESRequest() : 
+class ESRequest() :
     def __init__(self, esUrl = esUrl, index = index, proxies = proxies, size = size) :
         self.esUrl = esUrl
         self.index = index
@@ -38,36 +42,29 @@ class ESRequest() :
         self.es = Elasticsearch(
             [self.esUrl],
             connection_class = self.Connection,
-            proxies = proxies, 
+            proxies = proxies,
             size = size
         )
 
     class Connection(MyConnection) :
         pass
 
-    # def testConnection(self) :
-    #     try : 
-    #         return requests.get(self.esUrl).json()
-    #     except Exception as e : 
-    #         return e  
-
-# Instaciate connection
 
 esr = ESRequest()
 es = esr.es
 
-class Record : 
-    def __init__(self, record, es = es) : 
-        self.record = record  
+class Record :
+    def __init__(self, record, es = es) :
+        self.record = record
         self.es = es
         self.dupList = []
 
-    def myStemmer(self, sent) : 
+    def myStemmer(self, sent) :
         lang = detect(sent)
         tokens = ([fr.stem(word) for word in sent.split()],[en.stem(word) for word in sent.split()])[lang == "fr"]
         return " ".join([x for x in tokens if x not in titleStopwords])
 
-    def query(self) : 
+    def query(self) :
         titleDefault = self.record['title']['default']
         req = self.myStemmer(titleDefault)
         body = {
@@ -84,52 +81,49 @@ class Record :
                         }]
                         }
                     }
-                } 
+                }
 
         res = self.es.search(index =index, body = body)
         return res
 
 
     def deduplicate(self) :
-        
-        try : 
-            duplicatesIdConditor = [x["idConditor"] for x in self.record["duplicates"]] 
-        except : 
-            duplicatesIdConditor = []
 
-            
-        res = self.query()
-        if not res['hits']['hits'] : 
+        try :
+            duplicatesIdConditor = [x["idConditor"] for x in self.record["duplicates"]]
+            res = self.query()
+            if not res['hits']['hits'] :
+                return {'duplicates' : self.dupList}
+            else :
+                for i, rc in enumerate(res["hits"]["hits"]) :
+                    dupCandidateRecord = rc["_source"]
+                    comp = NoticeComparison(self.record, dupCandidateRecord)
+                    comp.run()
+                    if comp.result == 1 :
+                        if dupCandidateRecord['idConditor'] not in duplicatesIdConditor :
+                            self.dupList.append(
+                                {
+                                    "idConditor" : dupCandidateRecord['idConditor'],
+                                    "sourceUid" : dupCandidateRecord['sourceUid'],
+                                    "type" : dupCandidateRecord['typeConditor'],
+                                    "source" : dupCandidateRecord['source'],
+                                    "deduplicateRules" : comp.comment,
+                                }
+                            )
             return {'duplicates' : self.dupList}
-        else : 
-            for i, rc in enumerate(res["hits"]["hits"]) : 
-                dupCandidateRecord = rc["_source"]
-                comp = NoticeComparison(self.record, dupCandidateRecord)
-                comp.run()
-                if comp.result == 1 : 
-                    if dupCandidateRecord['idConditor'] not in duplicatesIdConditor : 
-                        self.dupList.append(
-                            {
-                                "idConditor" : dupCandidateRecord['idConditor'],
-                                "sourceUid" : dupCandidateRecord['sourceUid'],
-                                "type" : dupCandidateRecord['typeConditor'],
-                                "source" : dupCandidateRecord['source'],
-                                "comment" : comp.comment,
-                            }
-                        )
-
-            return {'duplicates' : self.dupList}
+        except Exception as err :
+            return {"error" : 100, "type" : err.__class__}
 
 
-if __name__ == "__main__" : 
+if __name__ == "__main__" :
     from time import time
     import json
     import sys
 
-    filename = "test.json"
-    with open(filename) as f : 
+    filename = "/home/dago/Documents/TDM/sprint-6/co-near_dup/test.json"
+    with open(filename) as f :
         datas = json.load(f)
-    for data in datas: 
+    for data in datas:
         a = time()
         record = Record(data)
         dup = record.deduplicate()
@@ -137,5 +131,5 @@ if __name__ == "__main__" :
         print(dup)
         print()
 
-        
+
 
