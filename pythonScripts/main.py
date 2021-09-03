e00bf194b8e5952a2650e48d799bdb8c88e736b2
amaelDago <amaeldago@gmail.com>
2021-09-03 16:13:07 +0200

    Entrypoint

---
+++ pythonScripts/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import os
+import argparse
+
+parentDirectory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, parentDirectory)
+
+
+from deduplicate.deduplicate import Record
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--docobject_string', type=str)
+args = parser.parse_args()
+
+corpus = json.loads(args.docobject_string)
+
+for data in corpus :
+    record = Record(data)
+    duplicate = record.deduplicate()
+    sys.stdout.write(json.dumps(str(duplicate)))
+    sys.stdout.write('\n')
