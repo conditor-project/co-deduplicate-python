f08aea731ce3c67b0f984686d379d2c8020c242e
amaelDago <amaeldago@gmail.com>
2021-12-06 11:29:17 +0100

    refactor: load environment variables

--- pythonScripts/deduplicate/deduplicate.py
+++ pythonScripts/deduplicate/deduplicate.py
@@ -1,76 +1,37 @@
-import os
-import sys
-import json
-# import pathlib
-from elasticsearch import RequestsHttpConnection
-from elasticsearch import Elasticsearch
-from nltk.stem import SnowballStemmer
-from nltk.stem import PorterStemmer
-from langdetect import detect
-import dotenv
-
-
-dotenv.load_dotenv()
-
+import re
 from . import utils
 from . import params
 
-
-# Instaciantye langdetctor
-fr = SnowballStemmer('french')
-en = PorterStemmer()
-
-
-# Set connection config
-ES_URL = os.environ.get("ES_URL", "http://localhost:9200")
-INDEX = os.environ.get("INDEX")
-DOC_TYPE = os.environ.get("RECORD")
-HTTPS_PROXY = os.environ.get("HTTPS_PROXY")
-HTTP_PROXY = os.environ.get("HTTP_PROXY")
-proxies = {"https" : HTTPS_PROXY, "http" : HTTP_PROXY}
-SIZE = os.environ["RESPONSE_SIZE"]
-
-
-# Elastic search requests using proxies
-class Connection(RequestsHttpConnection):
-    def __init__(self, *args, **kwargs):
-        proxies = kwargs.pop('proxies', {})
-        super(Connection, self).__init__(*args, **kwargs)
-        self.session.proxies = proxies
-
-
-def ES_request(es_url, connection_class, proxies, size) :
-    return Elasticsearch(
-        [es_url],
-        connection_class = connection_class,
-        proxies = proxies,
-        size = size
-    )
-
-# Instanciate elastic client and its request class
-es = ES_request(
-    es_url=ES_URL,
-    connection_class= Connection,
-    proxies = proxies,
-    size = SIZE
-)
-
-
-class Record :
-    def __init__(self, record, es = es) :
+class RecordDeduplicate :
+    def __init__(self, record, es, index) :
         self.record = record
         self.es = es
+        self.index = index
         self.dupList = []
         self.result_field = ["deduplicateRules", "idConditor", "source", "sourceUid"]
 
-    def myStemmer(self, sent) :
-        lang = detect(sent)
-        tokens = ([fr.stem(word) for word in sent.split()],[en.stem(word) for word in sent.split()])[lang == "fr"]
-        return " ".join([x for x in tokens if x not in params.titleStopwords])
+    def preprocess(self, sent) :
+        """
+        split a sentence, remove stopwords and join token
+        :param sentence : a sequence of tokens
+        :return preprocessed sentence : sentence where stop word is removed
+        """
+        if not isinstance(sent, str) :
+            sent = str(sent)
+        tokens = [token for token in re.findall("\w+", sent) \
+            if token not in params.titleStopwords]
+        return " ".join(tokens)
 
     def query(self) :
-        titleDefault = self.record['title']['default']
-        req = self.myStemmer(titleDefault)
+        """
+        For a given sentence, preprocess and query elastic
+        :param sentence
+        :return elasticsearch responses
+        see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-your-data.html
+        for details.
+        """
+        default_title = self.record['title']['default']
+        req = self.preprocess(default_title)
         body = {
             'query': {
                 'bool': {
@@ -79,7 +40,7 @@ class Record :
                             'title.default': {
                             'query' : req,
                             'fuzziness': "AUTO",
-                            'minimum_should_match': '70%'
+                            'minimum_should_match': '60%'
                                 }
                             }
                         }]
@@ -87,29 +48,36 @@ class Record :
                     }
                 }
 
-        res = self.es.search(index = INDEX, body = body)
+        res = self.es.search(index = self.index, body = body)
         return res
 
 
     def deduplicate(self) :
+        """
+        Deduplicate function.
+        :param record : bibliographic record
+        :return duplicates if exists. duplicates are list of dict. each dict contains :
+         - idConditor
+         - source
+         - sourceUid
+         - decision
+         - deduplicateRules
+        """
         if not isinstance(self.record, dict) :
             return {"error" : {"code" : 104, "type" : "<class 'Type'>", "message" : "Must be an Object"}}
 
         if not bool(self.record):
             return {"error" : {"code" : 103, "type" : "<class 'ValueError'>", "message" : "Empty object"}}
 
-        if "idConditor" not in self.record :
-            return {"error" : {"code" : 102, "type" : "<class 'KeyError'>", "message" : "KeyError('idConditor')"}}
-
         if "sourceUid" not in self.record :
             return {"error" : {"code" : 101, "type" : "<class 'KeyError'>", "message" : "KeyError('sourceUid')"}}
 
-
+        # take idConditor for duplicates if exists
+        duplicatesIdConditor = []
         try :
             duplicatesIdConditor = [x["idConditor"] for x in self.record["duplicates"]]
         except Exception as err :
-            return {"error" : {"code" : 100, "type" : err.__class__, "message" : err}}
-
+            pass
 
         if "idConditor" in self.record :
             duplicatesIdConditor.append(self.record["idConditor"])
@@ -128,7 +96,7 @@ class Record :
                             dico[field] = dupCandidateRecord[field]
                         except : pass
 
-                    dico["deduplicateRules"] = comp.comment
+                    dico["deduplicateRules"] = comp.duplicate_rules
                     dico['type'] = dupCandidateRecord["typeConditor"]
                     self.dupList.append(dico)
 
